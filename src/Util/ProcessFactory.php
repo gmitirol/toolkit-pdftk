bfcb352f7a4e684f3ea99c740967ab6485838582
Andreas Erhard <andreas.erhard@i-med.ac.at>
2019-01-24 12:10:11 +0100

    Fix deprecation notice with latest version of symfony/process
    Support old (Symfony 2.7 to 4.1) and new (Symfony 4.2+) syntax to build Symfony Process instances from a command line.

--- src/Util/ProcessFactory.php
+++ src/Util/ProcessFactory.php
@@ -3,6 +3,7 @@
  * Process factory.
  *
  * @copyright 2014-2018 Institute of Legal Medicine, Medical University of Innsbruck
+ * @author Andreas Erhard <andreas.erhard@i-med.ac.at>
  * @author Nikola Vrlazic <nikola.vrlazic@i-med.ac.at>
  * @license LGPL-3.0-only
  * @link http://www.gerichtsmedizin.at/
@@ -30,6 +31,21 @@ class ProcessFactory
      */
     public function createProcess($commandLine)
     {
-        return new Process($commandLine);
+        // support old (Symfony 2.7 to 4.1) and new (Symfony 4.2+) syntax to build Symfony Process instances.
+        $process = $this->useNewSyntax() ? Process::fromShellCommandline($commandLine) : new Process($commandLine);
+
+        return $process;
+    }
+
+    /**
+     * Returns whether the new syntax to build Symfony Process instances from a command line should be used.
+     *
+     * @see https://github.com/symfony/symfony/pull/27821
+     *
+     * @return bool
+     */
+    private function useNewSyntax()
+    {
+        return method_exists(Process::class, 'fromShellCommandline');
     }
 }
